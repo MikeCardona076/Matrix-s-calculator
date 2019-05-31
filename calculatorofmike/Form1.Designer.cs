9cb7d200b80abcd4d8900a012ef922953c80313b
MikeCardona076 <mikecardona076@gmail.com>
2019-05-31 18:39:50 +0200

    first commit

---
+++ calculatorofmike/Form1.Designer.cs
@@ -0,0 +1,423 @@
+﻿namespace calculatorofMike
+{
+    partial class CUADROPRINCIPAL
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.matriz1 = new System.Windows.Forms.Panel();
+            this.A1 = new System.Windows.Forms.TextBox();
+            this.B1 = new System.Windows.Forms.TextBox();
+            this.C1 = new System.Windows.Forms.TextBox();
+            this.A2 = new System.Windows.Forms.TextBox();
+            this.B2 = new System.Windows.Forms.TextBox();
+            this.C2 = new System.Windows.Forms.TextBox();
+            this.A3 = new System.Windows.Forms.TextBox();
+            this.B3 = new System.Windows.Forms.TextBox();
+            this.C3 = new System.Windows.Forms.TextBox();
+            this.resultadomatriz = new System.Windows.Forms.Panel();
+            this.I3 = new System.Windows.Forms.TextBox();
+            this.H3 = new System.Windows.Forms.TextBox();
+            this.G3 = new System.Windows.Forms.TextBox();
+            this.I2 = new System.Windows.Forms.TextBox();
+            this.H2 = new System.Windows.Forms.TextBox();
+            this.G2 = new System.Windows.Forms.TextBox();
+            this.I1 = new System.Windows.Forms.TextBox();
+            this.H1 = new System.Windows.Forms.TextBox();
+            this.G1 = new System.Windows.Forms.TextBox();
+            this.matriz2 = new System.Windows.Forms.Panel();
+            this.F3 = new System.Windows.Forms.TextBox();
+            this.E3 = new System.Windows.Forms.TextBox();
+            this.D3 = new System.Windows.Forms.TextBox();
+            this.F2 = new System.Windows.Forms.TextBox();
+            this.E2 = new System.Windows.Forms.TextBox();
+            this.D2 = new System.Windows.Forms.TextBox();
+            this.F1 = new System.Windows.Forms.TextBox();
+            this.E1 = new System.Windows.Forms.TextBox();
+            this.D1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.matriz1.SuspendLayout();
+            this.resultadomatriz.SuspendLayout();
+            this.matriz2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.label1.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(288, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MIKE\'S CALCULATOR";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // matriz1
+            // 
+            this.matriz1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.matriz1.Controls.Add(this.C3);
+            this.matriz1.Controls.Add(this.B3);
+            this.matriz1.Controls.Add(this.A3);
+            this.matriz1.Controls.Add(this.C2);
+            this.matriz1.Controls.Add(this.B2);
+            this.matriz1.Controls.Add(this.A2);
+            this.matriz1.Controls.Add(this.C1);
+            this.matriz1.Controls.Add(this.B1);
+            this.matriz1.Controls.Add(this.A1);
+            this.matriz1.Location = new System.Drawing.Point(48, 67);
+            this.matriz1.Name = "matriz1";
+            this.matriz1.Size = new System.Drawing.Size(277, 164);
+            this.matriz1.TabIndex = 2;
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(16, 15);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(57, 20);
+            this.A1.TabIndex = 3;
+            this.A1.TextChanged += new System.EventHandler(this.A1_TextChanged);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(105, 15);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(57, 20);
+            this.B1.TabIndex = 4;
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(194, 15);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(57, 20);
+            this.C1.TabIndex = 5;
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(16, 68);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(57, 20);
+            this.A2.TabIndex = 6;
+            this.A2.TextChanged += new System.EventHandler(this.A2_TextChanged);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(105, 68);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(57, 20);
+            this.B2.TabIndex = 7;
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(194, 68);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(57, 20);
+            this.C2.TabIndex = 8;
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(16, 118);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(57, 20);
+            this.A3.TabIndex = 4;
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(105, 118);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(57, 20);
+            this.B3.TabIndex = 4;
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(194, 118);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(57, 20);
+            this.C3.TabIndex = 4;
+            // 
+            // resultadomatriz
+            // 
+            this.resultadomatriz.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.resultadomatriz.Controls.Add(this.I3);
+            this.resultadomatriz.Controls.Add(this.H3);
+            this.resultadomatriz.Controls.Add(this.G3);
+            this.resultadomatriz.Controls.Add(this.I2);
+            this.resultadomatriz.Controls.Add(this.H2);
+            this.resultadomatriz.Controls.Add(this.G2);
+            this.resultadomatriz.Controls.Add(this.I1);
+            this.resultadomatriz.Controls.Add(this.H1);
+            this.resultadomatriz.Controls.Add(this.G1);
+            this.resultadomatriz.Location = new System.Drawing.Point(326, 290);
+            this.resultadomatriz.Name = "resultadomatriz";
+            this.resultadomatriz.Size = new System.Drawing.Size(277, 164);
+            this.resultadomatriz.TabIndex = 3;
+            this.resultadomatriz.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // I3
+            // 
+            this.I3.Location = new System.Drawing.Point(194, 118);
+            this.I3.Name = "I3";
+            this.I3.Size = new System.Drawing.Size(57, 20);
+            this.I3.TabIndex = 4;
+            // 
+            // H3
+            // 
+            this.H3.Location = new System.Drawing.Point(105, 118);
+            this.H3.Name = "H3";
+            this.H3.Size = new System.Drawing.Size(57, 20);
+            this.H3.TabIndex = 4;
+            // 
+            // G3
+            // 
+            this.G3.Location = new System.Drawing.Point(16, 118);
+            this.G3.Name = "G3";
+            this.G3.Size = new System.Drawing.Size(57, 20);
+            this.G3.TabIndex = 4;
+            // 
+            // I2
+            // 
+            this.I2.Location = new System.Drawing.Point(194, 68);
+            this.I2.Name = "I2";
+            this.I2.Size = new System.Drawing.Size(57, 20);
+            this.I2.TabIndex = 8;
+            // 
+            // H2
+            // 
+            this.H2.Location = new System.Drawing.Point(105, 68);
+            this.H2.Name = "H2";
+            this.H2.Size = new System.Drawing.Size(57, 20);
+            this.H2.TabIndex = 7;
+            // 
+            // G2
+            // 
+            this.G2.Location = new System.Drawing.Point(16, 68);
+            this.G2.Name = "G2";
+            this.G2.Size = new System.Drawing.Size(57, 20);
+            this.G2.TabIndex = 6;
+            // 
+            // I1
+            // 
+            this.I1.Location = new System.Drawing.Point(194, 15);
+            this.I1.Name = "I1";
+            this.I1.Size = new System.Drawing.Size(57, 20);
+            this.I1.TabIndex = 5;
+            // 
+            // H1
+            // 
+            this.H1.Location = new System.Drawing.Point(105, 15);
+            this.H1.Name = "H1";
+            this.H1.Size = new System.Drawing.Size(57, 20);
+            this.H1.TabIndex = 4;
+            // 
+            // G1
+            // 
+            this.G1.Location = new System.Drawing.Point(16, 15);
+            this.G1.Name = "G1";
+            this.G1.Size = new System.Drawing.Size(57, 20);
+            this.G1.TabIndex = 3;
+            // 
+            // matriz2
+            // 
+            this.matriz2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.matriz2.Controls.Add(this.F3);
+            this.matriz2.Controls.Add(this.E3);
+            this.matriz2.Controls.Add(this.D3);
+            this.matriz2.Controls.Add(this.F2);
+            this.matriz2.Controls.Add(this.E2);
+            this.matriz2.Controls.Add(this.D2);
+            this.matriz2.Controls.Add(this.F1);
+            this.matriz2.Controls.Add(this.E1);
+            this.matriz2.Controls.Add(this.D1);
+            this.matriz2.Location = new System.Drawing.Point(599, 67);
+            this.matriz2.Name = "matriz2";
+            this.matriz2.Size = new System.Drawing.Size(277, 164);
+            this.matriz2.TabIndex = 9;
+            // 
+            // F3
+            // 
+            this.F3.Location = new System.Drawing.Point(194, 118);
+            this.F3.Name = "F3";
+            this.F3.Size = new System.Drawing.Size(57, 20);
+            this.F3.TabIndex = 4;
+            // 
+            // E3
+            // 
+            this.E3.Location = new System.Drawing.Point(105, 118);
+            this.E3.Name = "E3";
+            this.E3.Size = new System.Drawing.Size(57, 20);
+            this.E3.TabIndex = 4;
+            // 
+            // D3
+            // 
+            this.D3.Location = new System.Drawing.Point(16, 118);
+            this.D3.Name = "D3";
+            this.D3.Size = new System.Drawing.Size(57, 20);
+            this.D3.TabIndex = 4;
+            // 
+            // F2
+            // 
+            this.F2.Location = new System.Drawing.Point(194, 68);
+            this.F2.Name = "F2";
+            this.F2.Size = new System.Drawing.Size(57, 20);
+            this.F2.TabIndex = 8;
+            // 
+            // E2
+            // 
+            this.E2.Location = new System.Drawing.Point(105, 68);
+            this.E2.Name = "E2";
+            this.E2.Size = new System.Drawing.Size(57, 20);
+            this.E2.TabIndex = 7;
+            // 
+            // D2
+            // 
+            this.D2.Location = new System.Drawing.Point(16, 68);
+            this.D2.Name = "D2";
+            this.D2.Size = new System.Drawing.Size(57, 20);
+            this.D2.TabIndex = 6;
+            // 
+            // F1
+            // 
+            this.F1.Location = new System.Drawing.Point(194, 15);
+            this.F1.Name = "F1";
+            this.F1.Size = new System.Drawing.Size(57, 20);
+            this.F1.TabIndex = 5;
+            // 
+            // E1
+            // 
+            this.E1.Location = new System.Drawing.Point(105, 15);
+            this.E1.Name = "E1";
+            this.E1.Size = new System.Drawing.Size(57, 20);
+            this.E1.TabIndex = 4;
+            // 
+            // D1
+            // 
+            this.D1.Location = new System.Drawing.Point(16, 15);
+            this.D1.Name = "D1";
+            this.D1.Size = new System.Drawing.Size(57, 20);
+            this.D1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "SUMA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "RESTA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(431, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "MULTIPLICACION";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CUADROPRINCIPAL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(960, 476);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.matriz2);
+            this.Controls.Add(this.resultadomatriz);
+            this.Controls.Add(this.matriz1);
+            this.Controls.Add(this.label1);
+            this.Name = "CUADROPRINCIPAL";
+            this.Text = "MIKE\'S CALCULATOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.matriz1.ResumeLayout(false);
+            this.matriz1.PerformLayout();
+            this.resultadomatriz.ResumeLayout(false);
+            this.resultadomatriz.PerformLayout();
+            this.matriz2.ResumeLayout(false);
+            this.matriz2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel matriz1;
+        private System.Windows.Forms.TextBox C3;
+        private System.Windows.Forms.TextBox B3;
+        private System.Windows.Forms.TextBox A3;
+        private System.Windows.Forms.TextBox C2;
+        private System.Windows.Forms.TextBox B2;
+        private System.Windows.Forms.TextBox A2;
+        private System.Windows.Forms.TextBox C1;
+        private System.Windows.Forms.TextBox B1;
+        private System.Windows.Forms.TextBox A1;
+        private System.Windows.Forms.Panel resultadomatriz;
+        private System.Windows.Forms.TextBox I3;
+        private System.Windows.Forms.TextBox H3;
+        private System.Windows.Forms.TextBox G3;
+        private System.Windows.Forms.TextBox I2;
+        private System.Windows.Forms.TextBox H2;
+        private System.Windows.Forms.TextBox G2;
+        private System.Windows.Forms.TextBox I1;
+        private System.Windows.Forms.TextBox H1;
+        private System.Windows.Forms.TextBox G1;
+        private System.Windows.Forms.Panel matriz2;
+        private System.Windows.Forms.TextBox F3;
+        private System.Windows.Forms.TextBox E3;
+        private System.Windows.Forms.TextBox D3;
+        private System.Windows.Forms.TextBox F2;
+        private System.Windows.Forms.TextBox E2;
+        private System.Windows.Forms.TextBox D2;
+        private System.Windows.Forms.TextBox F1;
+        private System.Windows.Forms.TextBox E1;
+        private System.Windows.Forms.TextBox D1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
